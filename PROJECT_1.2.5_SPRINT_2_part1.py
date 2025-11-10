dcb467f8651187eb6c9c17f1925fcebca3e12e77
Kashvi Gupta <guptakashvi26@gmail.com>
2025-11-10 15:26:17 -0800

    next page after user chooses where to go

---
+++ PROJECT_1.2.5_SPRINT_2_part1.py
@@ -0,0 +1,95 @@
+import turtle as trtl
+import time
+
+#----Initialize turtles----
+wn = trtl.Screen()
+player = trtl.Turtle()
+image_file = "the_turtle.gif"
+
+choice1 = trtl.Turtle()
+choice1.hideturtle()
+cave_image = "cave_choice.gif"
+choice2 = trtl.Turtle()
+choice2.hideturtle()
+waterfall_image = "waterfall_choice.gif"
+queen_image = 'ocean_queen.gif'
+king_image = 'burrow_king.gif'
+wn.register_shape(queen_image)
+wn.register_shape(image_file)
+wn.register_shape(cave_image)
+wn.register_shape(waterfall_image)
+wn.register_shape(king_image)
+
+choice1.shape(cave_image)
+choice2.shape(waterfall_image)
+player.shape(image_file)
+
+player.penup()
+player.goto(0, -200)
+
+wn.bgpic('default.gif')
+
+intro = trtl.Turtle()
+style = ('Caveat', 15, 'bold')
+intro.penup()
+intro.hideturtle()
+intro.goto(20, 80)
+intro.write("Welcome, player, to Turtle quest! \nMy name's Trotter and you are\ngoing to guide me on our adventure today! \nYou will be given two options as we\nmove through the land. There is a dark overlord \nin these parts and he's out for \nour town next! We must stop him! \n(click s to begin)", font=style, align='center')
+
+#----Turtle onclick methods----
+terminate = False
+
+def begin():
+    global terminate
+    if not terminate:
+        player.goto(800, -200)
+        wn.bgpic('panel-2.gif')
+        intro.clear()
+        player.hideturtle()
+        player.goto(-800, -100)
+        player.showturtle()
+        player.goto(-200, -100)
+        intro.pencolor("white")
+        intro.goto(20, 150)
+        intro.write("Welcome to the waterfall meadow! To stop the \ndark overlord, we will need some help!", font=style, align = 'center')
+        intro.goto(20, 80)
+        time.sleep(1)
+        intro.write("\n\n\n\nI heard that the river queen doesn't like \nhim! But also, the burrow king doesn't \nlike him either! Who should we ask for help?", font=style, align='center')
+        choice1.penup()
+        choice1.goto(-250, -200)
+        choice1.showturtle()
+        choice2.penup()
+        choice2.goto(250, -200)
+        choice2.showturtle()
+        terminate = True
+
+def waterfall(x, y):
+    intro.clear()
+    choice1.hideturtle()
+    choice2.hideturtle()
+    player.goto(250,-200)
+    choice1.shape(queen_image)
+    choice1.goto(300, 100)
+    choice1.showturtle()
+    wn.bgpic('underwater.gif')
+
+def cave(x, y):
+    intro.clear()
+    choice1.hideturtle()
+    choice2.hideturtle()
+    player.goto(-250, 200)
+    choice1.shape(king_image)
+    choice1.goto(-300, 100)
+    choice1.showturtle()
+    player.hideturtle()
+    player.goto(500, 100)
+    time.sleep(1)
+    player.goto(200, 100)
+    wn.bgpic('cave_bg.gif')
+
+wn.onkeypress(begin, "s")
+choice1.onclick(cave)
+choice2.onclick(waterfall)
+
+wn.listen()
+wn.mainloop()
